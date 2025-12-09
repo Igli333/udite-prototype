cdd0136bfd9b1aabcca8e7e34bff12c5853c1b4f
Igli Balla <igliballa312@gmail.com>
2025-12-09 16:52:39 +0100

    Base resources set up and functional

--- ms-elt/app.py
+++ ms-elt/app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return 'Hello World! This is the ELT of UDiTE'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5002, debug=True)
--- ms-simulator/app.py
+++ ms-simulator/app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return 'Hello World! This is the simulator of UDiTE'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
