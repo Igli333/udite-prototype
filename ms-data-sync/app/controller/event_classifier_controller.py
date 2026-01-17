3cdd38ead87a6980417f0aa82c50a638b054b51b
Igli Balla <igliballa312@gmail.com>
2026-01-17 18:58:11 +0100

    Removal of controller

--- ms-data-sync/app/controller/event_classifier_controller.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, jsonify
-from .. import service
-
-event_classifier = Blueprint('event_classifier', __name__)
-sync_service = service.DataSyncService()
-
-@event_classifier.route('', methods=['GET'])
-def pff():
-    pass
